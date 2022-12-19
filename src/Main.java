85112a30809cef7b9897c58a95660ef6812df4ef
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-19 22:33:43 +0300

    work8 сделано 4-8 задания

--- src/Main.java
+++ src/Main.java
@@ -37,5 +37,44 @@ public class Main {
             population = population - deathPepYear;
             System.out.println("Год " + i + " , численность населения составляет " + population);
         }
+
+        //task4,5
+        System.out.println("задание 4,5");
+        float vklad = 15_000;
+        int monthNum = 1;
+        while (vklad <= 12_000_000) {
+            vklad = vklad + (vklad * 7) / 100;
+            if (monthNum % 6 == 0) {
+                System.out.println("За " + monthNum + " месяц, накопилось " + vklad);
+            }
+            monthNum++;
+        }
+
+        //task6
+        System.out.println("задание 6");
+        double vklad2 = 15_000;
+        for (int i = 1; i <= 9 * 12; i = i + 6) {
+            double sumNew = vklad2;
+            vklad2 = (vklad2 * 1.07) * 6;
+            System.out.println("За " + i + " месяц, накопилось " + sumNew);
+        }
+
+        //task7
+        System.out.println("Задание 7");
+        int friday = 7;
+        for (int i = friday; i <= 31; i = i + 7) {
+            System.out.println("пятница " + i + " число");
+        }
+
+        //task8
+        System.out.println("задание 8");
+        int thisYear = 2022;
+        int beforeYear = thisYear - 200;
+        int afterYear = thisYear + 100;
+        for (int i = beforeYear; i <= afterYear; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
